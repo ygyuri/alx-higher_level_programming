2a950c44a2395c9cf665cb2a2ab05f539cff8f23
Ygyuri <gideonyuri15@gmail.com>
2023-06-08 15:50:34 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,16 @@
-Write a program that prints the alphabet in uppercase, followed by a new line.
+#!/usr/bin/python3
 
-    Your program should be maximum 3 lines long
-    You are not allowed to use:
-        any loops
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
+
