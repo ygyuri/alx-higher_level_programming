b2dacfcae50e7eb5ef1cf65d7e27a113c59a25f7
Ygyuri <gideonyuri15@gmail.com>
2023-06-13 01:44:12 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
 
+"""
+This module demonstrates the sum of two numbers.
+"""
+
+from add_0 import add
+
+def main():
+    """Print the sum of 1 and 2."""
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{a} + {b} = {add(a, b)}")
 
+if __name__ == "__main__":
+    main()
 
