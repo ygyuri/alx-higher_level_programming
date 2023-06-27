33945ab4bb39a88d623c77ec259c813e7620003c
Ygyuri <gideonyuri15@gmail.com>
2023-06-27 14:13:17 -0700

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square with size"""
+
+
+class Square:
+    """Class to create a square with its size"""
+
+    def __init__(self, size):
+        """Constructor of the Square"""
+        self.__size = size
