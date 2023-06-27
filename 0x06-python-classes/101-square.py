7d2c8ff83760d9a2378694465d5786b42d3a3f0c
Ygyuri <gideonyuri15@gmail.com>
2023-06-27 14:26:15 -0700

    updated

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -12,8 +12,20 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Constructor of a Square with the size and position"""
-        self.size = size
-        self.position = position
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+        if (len(position) != 2) or (type(position) is not tuple)\
+                or (type(position[0]) is not int)\
+                or (type(position[1]) is not int)\
+                or (position[0] < 0) or (position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
 
     def area(self):
         """Method to get the area of the Square"""
@@ -29,6 +41,20 @@ class Square:
             for rows in range(self.__size):
                 print(" " * self.position[0], end='')
                 print("#" * self.__size)
+
+    def __str__(self):
+        str = ""
+        if (self.__size == 0):
+            return str
+        else:
+            for blank in range(self.position[1]):
+                str += "\n"
+            for rows in range(self.__size):
+                str += (" " * self.position[0])
+                str += ("#" * self.__size)
+                if (rows != self.__size - 1):
+                    str += "\n"
+        return (str)
 
     @property
     def size(self):
