151ddc33fc06be25a1288fd7e89f14f29853dc37
Ygyuri <gideonyuri15@gmail.com>
2023-09-12 13:04:51 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  let nOccurrences = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (searchElement === list[i]) {
-      nOccurrences++;
-    }
-  }
-  return nOccurrences;
-};
 
+const nbOccurences = (list, searchElement) => list.filter((element) => searchElement === element).length;
+
+module.exports = { nbOccurences };
